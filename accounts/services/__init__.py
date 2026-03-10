6f2d52cabe09823b9e8b68915d148f5ecf15b23f
Your Name <gpp@seger.es.gov.br>
2026-03-10 11:21:05 -0300

    retirada da pasta e arquivos

--- accounts/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .authorization_service import AuthorizationService, get_authorization_service
-from .token_service import TokenService, get_token_service
-
-__all__ = [
-    "TokenService",
-    "get_token_service",
-    "AuthorizationService",
-    "get_authorization_service",
-]
